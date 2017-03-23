f752e4d15db88766e3a8e5637551018d2e573327
deeksha <deeksha.krishnar@gmail.com>
2017-03-23 18:00:47 +0530

    hello

--- emamples.py
+++ emamples.py
@@ -1,1 +1,39 @@
-print('hello world!')
+#print('hello world')
+
+#if(3>2):
+    #print("it works")
+
+#name= 'abc'
+#if name=='ola':
+#    print('ola')
+#if name=='def'
+#    print('def')
+#else:
+    #print('abc')
+
+#volume=100
+#if volume<20 or volume>80:
+    #volume=50
+    #print('true')
+
+#for i in range(1,6):
+    #print(i)
+
+    #import math
+        #math.sqrt(10)
+
+#def hi():
+    #print('hi there')
+    #print('how are you')
+#hi()
+
+#class parent():
+    #def __init__(self, last_name, eye_color):
+    #self.last_name = last_name
+    #self.eye_color = eye_color
+
+#diksha_krishna = parent("krishna","blue")
+#print(diksha_krishna.last_name)
+
+from math import *
+sqrt.num(10)
