0c4121f62600df28a64ca758e696d7babe02dc0d
deeksha <deeksha.krishnar@gmail.com>
2017-03-24 10:58:59 +0530

    calculator

---
+++ practice.py
@@ -0,0 +1,41 @@
+def add(x,y):
+#"addition"
+    return x+y
+
+def subtract(x,y):
+#"subtraction"
+    return x-y
+
+def multiply(x,y):
+#"multiplication"
+    return x*y
+
+def divide(x,y):
+#"division"
+    return x/y
+
+print("enter choice")
+print("1.add")
+print("2.sub")
+print("3.mul")
+print("4.div")
+
+choice= input("enter choice(1/2/3/4):")
+
+num1= int(input("enter number1:"))
+num2= int(input("enter number2:"))
+
+
+if choice == '1':
+    print(num1,"+",num2,"=", add(num1,num2))
+
+elif choice == '2':
+    print(num1,"-",num2,"=", subtract(num1,num2))
+
+elif choice == '3':
+    print(num1,"*",num2,"=", multiply(num1,num2))
+
+elif choice == '4':
+    print(num1,"/",num2,"=", divide(num1,num2))
+else:
+    print("invalid")
