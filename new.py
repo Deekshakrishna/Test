1bd0f704398f54aca18841eeac6f6a3d579f9e1b
deeksha <deeksha.krishnar@gmail.com>
2017-03-24 10:26:20 +0530

    new programs

---
+++ new.py
@@ -0,0 +1,56 @@
+#num1=5
+#num2=10
+
+#num1=num2
+#num2=temp
+
+#print(num1,num2)
+
+
+
+#num=17
+#if num>1:
+   #for i in range(2,num):
+      # if num%i==0:
+           #print(num,"not a prime number")
+           #break
+   #else:
+      # print(num,"a prime number")
+#else:
+   #print(num,"is not a prime number")
+
+
+#n=454
+#sum=0
+
+#if n>0:
+
+    #r=n%10
+    #sum=(sum*10)+r
+    #n=n/10
+
+#else:
+    #print('exit')
+
+#if n==sum:
+    #print("palindrome")
+#else:
+    #print("not")
+
+
+num= 1234
+rev=0
+
+if num!=0:
+
+    rev=rev*10
+    rev=rev+num%10
+    num=num/10
+
+else:
+    print(rev,'exit')
+
+#if rev==num:
+#print(num,"success")
+#else:
+#print("fail")
